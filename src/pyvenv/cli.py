c5a754606520764bc3055071846c86ba5bf4c2d6
pumbas600 <joshjeffers600@gmail.com>
2023-11-28 22:26:56 +1100

    feat: :sparkles: Actually display the current venv if one is active

--- src/pyvenv/cli.py
+++ src/pyvenv/cli.py
@@ -1,5 +1,5 @@
 from typing import List
-from pyvenv.commands import create, current
+from pyvenv.commands import create_venv, display_current_venv
 
 
 DEFAULT_VENV_DIR = ".venv"
@@ -10,10 +10,10 @@ def handle_command(args: List[str]) -> None:
     arg_count = len(args)
 
     if arg_count == 0:
-        current()
+        display_current_venv()
         return
 
     command = args[0].lower()
     if command == 'create':
         venv_dir = args[1] if arg_count > 1 else DEFAULT_VENV_DIR
-        create(venv_dir)
+        create_venv(venv_dir)
--- src/pyvenv/commands.py
+++ src/pyvenv/commands.py
@@ -1,11 +1,16 @@
+import os
 import sys
 
 from venv import EnvBuilder
 from typing import Optional
 
 
-def create(venv_dir: str) -> None:
-    """Create a new virtual environment at the given directory."""
+def create_venv(venv_dir: str) -> None:
+    """
+    Create a new virtual environment at the given directory.
+    
+    venv_dir - The directory to create the virtual environment at.
+    """
     builder = EnvBuilder()
     builder.create(venv_dir)
 
@@ -14,9 +19,24 @@ def activate(venv_name: Optional[str]) -> None:
     pass
 
 
-def current() -> None:
-    print(sys.prefix)
-    print(_is_venv_active())
+def display_current_venv() -> None:
+    """
+    Display the current active virtual environment. If there is no virtual environment
+    active then this is indicated to the user.
+    """
+    if not _is_venv_active():
+        print("No virtual environment is currently active.")
+        return
+
+    # Include the separator at the end of the current working directory so that it gets
+    # removed by `str.removeprefix()`.
+    cwd = os.getcwd() + os.sep
+    venv_dir = sys.prefix
+
+    if venv_dir.startswith(cwd):
+        venv_dir = venv_dir.removeprefix(cwd)
+
+    print(f'Current virtual environment: {venv_dir}')
 
 
 def _is_venv_active() -> bool:
