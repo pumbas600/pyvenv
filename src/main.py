d6ab0a759db3941fe724076b4e21a260b8f48b27
pumbas600 <joshjeffers600@gmail.com>
2023-11-28 21:35:14 +1100

    feat: :sparkles: Create cli file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
-from pyvenv.commands import current
+import sys
+
+from pyvenv.cli import handle_command
 
 def main():
     """Venv entrypoint for the CLI."""
-    current()
+    args = sys.argv[1:]
+    handle_command(args)
 
 
 if __name__ == '__main__':
---
+++ src/pyvenv/cli.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+def handle_command(args: List[str]) -> None:
+    """Handle the command with the given CLI arguments."""
+    print(args)
